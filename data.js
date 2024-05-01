f3704d34b75cd74431f1c187e140263ecd68e3c3
cc <wyswy@gmail.com>
2024-05-01 16:20:30 +0800

    1. 初始化文件

---
+++ data.js
@@ -0,0 +1,17 @@
+let videoPaths = [
+"G.E.M.邓紫棋-一路逆风.mp4",
+"G.E.M.邓紫棋-倒数.mp4",
+"G.E.M.邓紫棋-光年之外-(电影《太空旅客》中文主题曲).mp4",
+"G.E.M.邓紫棋-再见.mp4",
+"G.E.M.邓紫棋-单行的轨道.mp4",
+"G.E.M.邓紫棋-多远都要在一起.mp4",
+"G.E.M.邓紫棋-奇迹.mp4",
+"G.E.M.邓紫棋-来自天堂的魔鬼.mp4",
+"G.E.M.邓紫棋-死了都要 • 爱.mp4",
+"G.E.M.邓紫棋-泡沫.mp4",
+"G.E.M.邓紫棋-那一夜.mp4",
+"04 G.E.M. 鄧紫棋 03 新的心跳 官方视频 flac音轨.mp4",
+];
+let baseIP = "";
+let basePath = "./GEM/";
+videoPaths = videoPaths.map(item=>baseIP+basePath+item);
